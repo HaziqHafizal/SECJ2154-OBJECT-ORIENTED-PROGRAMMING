196327b5206d841a52caf93d219a41f3debfceb9
Muhammad Haziq <152416635+HaziqHafizal@users.noreply.github.com>
2025-07-05 16:47:27 +0800

    Create Course.java

---
+++ OOP FINALS/CHAPTER 6/Course.java
@@ -0,0 +1,13 @@
+public class Course
+{
+    private String name;
+    private String code;
+    private int section;
+
+    public Course(String name, String code, String section)
+    {
+        this.name = name;
+        this.code = code;
+        this.section = section;
+    }
+}
